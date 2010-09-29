a2db36138c796e5c2e3e70075c166a28bd6aae76
yakisoba_yuki <yakisobay@gmail.com>
2010-09-30 00:02:32 +0900

    名前と誕生日がリスト化対応(レイアウト無視されている)

--- src/test/com/ContactPickerActivity.java
+++ src/test/com/ContactPickerActivity.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.CommonDataKinds.Event;
 import android.util.Log;
-import android.widget.TextView;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ContactPickerActivity extends Activity {
 	/** Called when the activity is first created. */
+	private ListView listView = null;
 	
 	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.main);
-
+		
 		try {
 			fillData();
 		} catch (InterruptedException e) {
@@ -26,30 +29,41 @@ public class ContactPickerActivity extends Activity {
 
 	// コンタクトデータを取得して表示
 	private void fillData() throws InterruptedException {
-		// 試しにこの選択文がどのように出力するのかDebug
-		//String d = Data.MIMETYPE + "=" + Event.CONTENT_ITEM_TYPE + " AND "+ Event.TYPE + "=" + Event.TYPE_BIRTHDAY;
-        
+		listView = (ListView) findViewById(R.id.list);
+
+        // ListViewに表示する要素を保持するアダプタを生成
+		// 【重要】これだと1つ目のテキストと2つ目のテキストが同じレイアウトになってしまう。
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                                R.layout.contactsname);
+		
 		Cursor c = managedQuery( Data.CONTENT_URI, 
 			    new String[]{Event.DISPLAY_NAME, Event.DATA}, 
 			    Data.MIMETYPE + "=? AND " + Event.TYPE  + "=?", 
 			    new String[]{Event.CONTENT_ITEM_TYPE , 
 				String.valueOf(Event.TYPE_BIRTHDAY)}, 
 			    Data.DISPLAY_NAME ); 
+
+		// 名前と誕生日のindexを取得して、出力の際に参照する
+		int nameIndex = c.getColumnIndex(Event.DISPLAY_NAME);
+		int birthIndex = c.getColumnIndex(Event.DATA);
 		
 		if( c != null ) { 
 			try { 
 				while( c.moveToNext() ) { 
-					TextView nameView = (TextView) findViewById(R.id.ContactsName);
-					TextView birthView = (TextView) findViewById(R.id.Birthday);
+					//TextView nameView = (TextView) findViewById(R.id.ContactsName);
+					//TextView birthView = (TextView) findViewById(R.id.Birthday);
 
-					String displayName 	= c.getString(0); //ユーザ名
-			        String date			= c.getString(1); //誕生日
+					String displayName 	= c.getString(nameIndex); //ユーザ名
+			        String date			= c.getString(birthIndex); //誕生日
 			        
 			        Log.d("Testoutput",displayName);
 			        Log.d("Testoutput",date);
 			        
-					nameView.setText(displayName);
-					birthView.setText(date);					 
+			        arrayAdapter.add(displayName);
+			        arrayAdapter.add(date);
+			        
+			        // アダプタを設定
+			        listView.setAdapter(arrayAdapter);
 			    } 
 			} finally { 
 				c.close(); 
