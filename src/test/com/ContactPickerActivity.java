13e3e6f2870249a7bc4a396ef677e70d22c4e452
yakisoba_yuki <yakisobay@gmail.com>
2010-11-03 17:29:52 +0900

    全体チェック用のCheckBox、情報取り込み用のボタンを追加

--- src/test/com/ContactPickerActivity.java
+++ src/test/com/ContactPickerActivity.java
@@ -16,17 +16,20 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ContactPickerActivity extends Activity/* implements OnClickListener*/{
+public class ContactPickerActivity extends Activity implements OnClickListener{
 	/** Called when the activity is first created. */
 	private List<ContactsStatus> mList = null;
 	private ContactAdapter mAdapter = null;
 	
-	CheckBox chk01; 
-
+	CheckBox chk01;
+	private CheckBox check_full;
+	private Button button_import;
+	
 	public class ContactsStatus {
 		private String displayName;
 		private String birthday;
@@ -61,12 +64,20 @@ public class ContactPickerActivity extends Activity/* implements OnClickListener
 
 		Log.d("Testoutput", "------------------アプリ開始------------------");
 		
+		/* 連絡帳から名前と誕生日を取得して格納 */
 		ListView listView = (ListView) findViewById(R.id.list);
 		fillData();
 
 		mAdapter = new ContactAdapter(this, R.layout.contactsname, mList);
-
 		listView.setAdapter(mAdapter);
+
+		/* 「全てチェック」ボタンが押された時の処理 */
+		check_full = (CheckBox) findViewById(R.id.CheckBox_full);
+		check_full.setOnClickListener(this);
+
+		/* 「取り込み」ボタンが押された時の処理 */
+		button_import = (Button) findViewById(R.id.button_import);
+		button_import.setOnClickListener(this);
 	}
 
 	public class ContactAdapter extends ArrayAdapter<ContactsStatus>/* implements OnClickListener*/{
@@ -100,17 +111,15 @@ public class ContactPickerActivity extends Activity/* implements OnClickListener
 				final CheckBox chk01 = (CheckBox) view.findViewById(R.id.CheckBox);
 	    		chk01.setChecked(item.getCheckFlag());
 	    		
-	    		Log.d("Testoutput", "List1件表示");
-	    		//chk01.setOnClickListener(this);
-	            
 	    		chk01.setOnClickListener(new OnClickListener() {
 	                public void onClick(View v) {
 	                	if(chk01.isChecked() == true) {
 	                		item.setCheckFlag(true);
+		            		Log.d("Testoutput", "1つチェックした");
 	                	}else{
 	                		item.setCheckFlag(false);
+		            		Log.d("Testoutput", "1つチェック外した");
 		                }
-	            		Log.d("Testoutput", "viewが違う");
 	                }
 	            });
 			}
@@ -141,9 +150,6 @@ public class ContactPickerActivity extends Activity/* implements OnClickListener
 					String displayName = c.getString(nameIndex); // ユーザ名
 					String date = c.getString(birthIndex); // 誕生日
 					
-					//Log.d("Testoutput", displayName);
-					//Log.d("Testoutput", date);
-
 					item.setDisplayName(displayName);
 					item.setBirth(date);
 					mList.add(item);
@@ -151,6 +157,47 @@ public class ContactPickerActivity extends Activity/* implements OnClickListener
 			} finally {
 				c.close();
 			}
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v == check_full){
+			/* 全てのチェックボックスに対し、チェックON/OFFを切り替えたい  */
+			Log.d("Testoutput", "チェックOn/OFFが押された！");
+
+			//リストから、チェックボックスの情報を取得したい
+
+			
+			
+			// ボタンおした時の表示名を変える
+			if(check_full.isChecked() == true){
+				CheckBox b = (CheckBox)v;
+				b.setText("チェック解除");
+			}else if(check_full.isChecked() == false){
+				CheckBox b = (CheckBox)v;
+				b.setText("全てチェック");
+			}
+
+		}else if(v == button_import){
+			/* チェックされたものを取り込む処理を実施  */
+			Log.d("Testoutput", "取り込みが押された！");
+
+			//リストから、チェックボックスの情報を取得したい
+			
+			/* これ的な処理がしたい
+			 * 	final ContactsStatus item = getItem(0);
+			 * 
+			 *  Log.d("Testoutput",Boolean.toString(item.getCheckFlag()));
+			 *  if(item.getDisplayName() != null){
+			 *	 Log.d("Testoutput",item.getDisplayName());			
+			 *  }else{
+			 *	 Log.d("Testoutput","nullってた");
+			 *  }
+			 */
+			
+		}else{
+			Log.d("Testoutput", "何か押された！");			
 		}
 	}
 }
